932232fd6588f2596c37564d02ef51721d621dcd
MatheusTannus <166193169+catzito@users.noreply.github.com>
2026-03-23 22:05:02 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,20 @@
+numero = float(input("Digite um número: "))
 
+while True:
+    operacao = input("Digite +, -, * ou / (ou sair): ")
+
+    if operacao == "sair":
+        break
+
+    outro = float(input("Digite outro número: "))
+
+    if operacao == "+":
+        numero = numero + outro
+    elif operacao == "-":
+        numero = numero - outro
+    elif operacao == "*":
+        numero = numero * outro
+    elif operacao == "/":
+        numero = numero / outro
+
+    print("Resultado:", numero)
